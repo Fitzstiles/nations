2035e796939ba0dab0481c29eb317400bedeb252
Austin <austinpaul606@gmail.com>
2022-12-07 09:46:27 +0100

    about us page

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Computer from "../components/Computer";
 import Forex from "../components/Forex";
+import Intercessor from "../components/Intercessor";
 
 const Goals = () => {
   return (
     <div>
       <Computer />
       <Forex />
+      <Intercessor />
     </div>
   );
 };
